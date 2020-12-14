c12030f331f890f4e32386fd65159668ece69508
Konstantin Rolf <konstantin.rolf@gmail.com>
2020-12-14 09:46:05 +0100

    Fixed result images

--- lib/rotues/route_endpoint.dart
+++ lib/rotues/route_endpoint.dart
@@ -10,6 +10,21 @@ import 'package:groningen_guide/kl/kl_endpoint.dart';
 import 'package:groningen_guide/kl_engine.dart';
 import 'package:groningen_guide/widgets/widget_title.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter/services.dart' show rootBundle;
+
+Future<ImageProvider> loadImageFromAsset(String image) async {
+  if (image.startsWith('http://') || image.startsWith('https://'))
+    return NetworkImage(image);
+
+  // load as asset
+  try {
+    var bytes = await rootBundle.load(image);
+    return MemoryImage(bytes.buffer.asUint8List());
+  } catch (_) {
+    print('Could not load bytes');
+    rethrow;
+  }
+}
 
 class EndpointWidget extends StatelessWidget {
   final KlEndpoint endpoint;
@@ -23,12 +38,32 @@ class EndpointWidget extends StatelessWidget {
         Container(
           height: 50.0,
           alignment: Alignment.center,
-          child: Text(
-            endpoint.name,
-            style: theme.textTheme.headline6,
-          ),
+          child: Text(endpoint.name, style: theme.textTheme.headline6),
         ),
-        Text(endpoint.description, style: theme.textTheme.bodyText2)
+        Text(endpoint.description, style: theme.textTheme.bodyText2),
+        if (endpoint.image != null)
+          Center(child: FutureBuilder<ImageProvider>(
+            future: loadImageFromAsset(endpoint.image),
+            builder: (context, snap) {
+              if (snap.hasError)
+                return Text('Error loading image ${endpoint.image}');
+              if (snap.hasData)
+                return Container(
+                  height: 250.0, width: 250.0,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      fit: BoxFit.contain,
+                      image: snap.data
+                    )
+                  ),
+                );
+              return Container(
+                width: 100.0, height: 100.0,
+                padding: EdgeInsets.all(20.0),
+                child: CircularProgressIndicator(),
+              );
+            },
+          ))
       ],
     );
   }
@@ -41,37 +76,35 @@ class EndpointDialog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var theme = Theme.of(context);
-    return Dialog(
-      child: Container(
-        width: 300, height: 300,
+    return AlertDialog(
+      title: Container(
+        width: 500, height: 300,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget> [
             Expanded(child: EndpointWidget(
               endpoint: endpoint,
             )),
-            Row(
-              children: <Widget> [
-                FlatButton(
-                  child: Text('Exit', style: theme.textTheme.button,),
-                  onPressed: () {
-                    Navigator.of(context).pop(true);
-                  },
-                ),
-                FlatButton(
-                  child: Text('Reset', style: theme.textTheme.button),
-                  onPressed: () {
-                    // resets the engine
-                    Provider.of<QuestionData>(context, listen: false).clear();
-                    Provider.of<KlEngine>(context, listen: false).clear();
-                    Navigator.of(context).pop(true);
-                  }
-                )
-              ]
-            )
           ]
         )
       ),
+      actions: [
+        //FlatButton(
+        //  child: Text('Exit', style: theme.textTheme.button,),
+        //  onPressed: () {
+        //    Navigator.of(context).pop(true);
+        //  },
+        //),
+        FlatButton(
+          child: Text('Reset'),
+          onPressed: () {
+            // resets the engine
+            Provider.of<QuestionData>(context, listen: false).clear();
+            Provider.of<KlEngine>(context, listen: false).clear();
+            Navigator.of(context).pop(true);
+          }
+        )
+      ],
     );
   }
 }
