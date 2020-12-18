0ff7bb5bebfeb2dca13185191d5c9e2de214394b
Konstantin Rolf <konstantin.rolf@gmail.com>
2020-12-18 17:08:37 +0100

    moved actions to separate file

--- lib/rotues/route_home.dart
+++ lib/rotues/route_home.dart
@@ -6,8 +6,7 @@
 /// Livia Regus (S3354970): l.regus@student.rug.nl
 
 import 'package:flutter/material.dart';
-import 'package:groningen_guide/kl_parser.dart';
-import 'package:groningen_guide/rotues/route_endpoint.dart';
+import 'package:groningen_guide/model_actions.dart';
 import 'package:groningen_guide/widgets/action_info.dart';
 import 'package:groningen_guide/widgets/action_inspector.dart';
 import 'package:groningen_guide/widgets/widget_title.dart';
@@ -22,82 +21,6 @@ import 'package:groningen_guide/widgets/widget_debugger.dart';
 /// The main screen of the [RouteHome]
 class MainScreen extends StatelessWidget {
   const MainScreen({Key key}) : super(key: key);
-
-  Future<void> _next(BuildContext context, QuestionData questionData) async {
-    // finds the engine provider in the widget tree
-    var engine = Provider.of<KlEngine>(context, listen: false);
-    // creates a snapshot from the current context model
-    var snapshot = engine.contextProvider.model.snapshot();
-
-    // evaluates all events that were selected
-    for (var i in questionData.selectedOptions())
-      engine.evaluateEvents(i.events);
-
-    // unloads the question and stores the context model snapshot
-    questionData.unloadQuestion(snapshot);
-
-    // inferes additional variables
-    engine.inference();
-    // checks if any goals have been reached
-    var endpoint = engine.checkEndpoints();
-
-    if (endpoint != null) {
-      // we reached an endpoint and want to show the dialog
-      var result = await showEndpointDialog(context, endpoint);
-      switch (result) {
-        case GoalDialogAction.Previous:
-          _previous(context, questionData); // TODO side effects
-          break;
-        case GoalDialogAction.Reset:
-          _reset(context, questionData);
-          break;
-      }
-    } else {
-      // we have to ask a new question and check which are available
-      var questions = engine.availableQuestions();
-      // evaluates which questions were already asked
-      var available = questions.where((question) {
-        return !questionData.containsQuestion(question);
-      }).toList();
-
-      if (available.isEmpty) {
-        // we don't have any new question to ask, jump to the general conclusion
-        Navigator.of(context).pushNamed('/end');
-      } else {
-        // loads the first question that is available
-        questionData.loadQuestion(
-          available.first, engine.contextProvider.model);
-      }
-    }
-  }
-
-  /// Loads the previous question
-  void _previous(BuildContext context, QuestionData questionData) {
-    var model = Provider.of<KlContextProvider>(context, listen: false);
-    // only goes back if we have a question we can go back to
-    if (questionData.previous.isNotEmpty) {
-      // returns the latest context snapshot and loads the previous question
-      var lastSnap = questionData.loadPreviousQuestion();
-      // loads the snapshot as actual context model to the engine
-      model.loadSnap(lastSnap);
-    }
-  }
-
-  /// Loads the first question
-  void _first(BuildContext context, QuestionData questionData) {
-    var engine = Provider.of<KlEngine>(context, listen: false);
-    engine.inference();
-    var questions = engine.availableQuestions();
-    if (questions.isEmpty)
-      Navigator.of(context).pushNamed('/end');
-    else
-      questionData.loadQuestion(questions.first, engine.contextProvider.model);
-  }
-
-  void _reset(BuildContext context, QuestionData questionData) {
-    questionData.clear();
-    Provider.of<KlEngine>(context, listen: false).clear();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -120,7 +43,7 @@ class MainScreen extends StatelessWidget {
                     width: 100.0,
                     child: RaisedButton(
                       child: const Text('Previous'),
-                      onPressed: () => _previous(context, questionData)
+                      onPressed: () => previousQuestion(context)
                     ),
                   ),
                   const SizedBox(width: 10.0,),
@@ -130,7 +53,7 @@ class MainScreen extends StatelessWidget {
                     width: 100.0,
                     child: RaisedButton(
                       child: const Text('Next'),
-                      onPressed: () => _next(context, questionData)
+                      onPressed: () => nextQuestion(context)
                     ),
                   )
                 ]
@@ -152,7 +75,7 @@ class MainScreen extends StatelessWidget {
                     alignment: Alignment.center,
                     child: Text('Start Inference Process'),
                   ),
-                  onPressed: () => _first(context, questionData)
+                  onPressed: () => firstQuestion(context)
                 )
               ]
             )
