499f8398f5a9b3611d1969ef94891fe93fc6c615
KonstantinRr <konstantin.rolf@gmail.com>
2021-01-13 14:12:36 +0000

    Deploying to gh-pages from  @ 0fd5afd40446be07242a3f3786a0fa2780656cb4 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -71,8 +71,8 @@ const RESOURCES = {
 "assets/AssetManifest.json": "63dd9b17d6c5f51f570c6bb8b243a449",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
-"index.html": "bb0ae5768d334ad5408a523493418f87",
-"/": "bb0ae5768d334ad5408a523493418f87",
+"index.html": "ab1e0d955dc9dfc34f5347de2351d7c0",
+"/": "ab1e0d955dc9dfc34f5347de2351d7c0",
 "version.json": "a6fc3fd6e84615acff6fd79e0b9601a7",
 "main.dart.js": "35bfb8e165f83aeff3f350310ad46908",
 "favicon.png": "72774a7b5a4a54dc7d8fe364fc7b3d8f"
